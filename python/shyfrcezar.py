2c65eee79df6651993446e311298d54274522bea
itcrowd007 <itcrowd007@gmail.com>
2021-08-13 16:44:50 +0300

    edit file shufrcezar

--- python/shyfrcezar.py
+++ python/shyfrcezar.py
@@ -1,11 +1,9 @@
 #print (input().split())
-x=input()
-import re
-print(re.sub(r'\s+', '_', re.sub(r'^\s+(.+?)\s+$', r'\1', input())))
-
-
-print (len(x))
-print (x)
+y=input()
+#import re
+#print(re.sub(r'\s+', '_', re.sub(r'^\s+(.+?)\s+$', r'\1', input())))
+#print (len(x))
+#print (x)
 #find1=str(input())
 #new_list=''
 #if find1 not in x:
@@ -15,3 +13,11 @@ print (x)
 #        if (x[i]==find1):
 #            new_list=new_list+str(i)+' '
 #    print (new_list)
+sdvig=str(input())
+y=input()
+alphavit=(" abcdefghijklmnopqrstuvwxyz")
+for x in alphavit:
+    for i in range(len(tekst):
+        if tekst(i)==x:
+            print(x)
+
