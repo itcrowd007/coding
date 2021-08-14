3b597bb197452f00bee94f48b8a2d75c87cd6fc6
tolliik <itcrowd007@gmail.com>
2021-08-15 00:18:15 +0300

    reshenie s funkciei

--- python/shyfrcezar.py
+++ python/shyfrcezar.py
@@ -1,31 +1,37 @@
-#print (input().split())
-#y=input()
-#import re
-#print(re.sub(r'\s+', '_', re.sub(r'^\s+(.+?)\s+$', r'\1', input())))
-#print (len(x))
-#print (x)
-#find1=str(input())
-#new_list=''
-#if find1 not in x:
-#    print ('None')
-#else:
-#    for i in range(len(x)):
-#        if (x[i]==find1):
-#            new_list=new_list+str(i)+' '
-#    print (new_list)
+#variant s funktsiei
+def cezar (sdvig, tekst):
+
+    alphavit=(" abcdefghijklmnopqrstuvwxyz")
+    result=('')
+    for i in range(len(tekst)):
+        for x in alphavit:
+            if tekst[i]==x:
+                num=alphavit.index(x)+sdvig
+                if num >= len(alphavit):
+                    num=num-len(alphavit)
+                result=result+alphavit[num]
+    return print (result)
+
 sdvig=int(input())
-tekst=input()
+text=input()
+
+cezar (sdvig, text)
+
+'''
+#1 variant resheniaya
+
+sdvig=int(input())
+text=input()
+
 alphavit=(" abcdefghijklmnopqrstuvwxyz")
-#print (alphavit[sdvig])
 result=('')
 for i in range(len(tekst)):
     for x in alphavit:
         if tekst[i]==x:
-            #print (alphavit.index(x))
             num=alphavit.index(x)+sdvig
-            
             if num >= len(alphavit):
                 num=num-len(alphavit)
             result=result+alphavit[num]
-print (result)
 
+print (result)
+'''
