7e87f7e7b33e4a1677f7455195714b564d750597
tolliik <itcrowd007@gmail.com>
2021-09-25 15:06:36 +0300

    coderank adding

---
+++ python/coderank/listcomprehension.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+z = int(input())
+#n = int(input())
+array2 = [[i, j, k] for i in [0, x, y, z] for j in [0, x, y, z] for k in [0, x, y, z]]
+#array1 = []
+
+#for i in [0, x, y, z]:
+#    for j in [0, x, y, z]:
+#        for k in [0, x, y, z]:
+#            array1.append([i, j, k])
+print (array2)
